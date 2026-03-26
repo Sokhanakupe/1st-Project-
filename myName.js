95f6a130da1c1f449f02ec2989509f17da706d2e
Sokhanakupe <127474556+Sokhanakupe@users.noreply.github.com>
2026-03-26 19:20:17 +0000

    HI

--- myName.js
+++ myName.js
@@ -1,3 +1,8 @@
 cd myProject to O
 git init
 git add myName.js
+git status 
+git diff
+git add nameOfMyFile.fileExtension
+git commit -m "The sentence I want associated with this commit message"
+git push origin master
